dffce458c40a5494786adf57edf0590c0c53797a
anissa williams <anissawilliams16387@gmail.com>
2022-06-07 18:48:38 -0500

    login

--- public/login.js
+++ public/login.js
@@ -1,13 +1,11 @@
 const loginForm = async function (event) {
     event.preventDefault();
-
-    //queryselector should equal username form field on login page
+      
     const username = document.querySelector('#username').value.trim();
 
     //queryselector should equal password form field on login page
     const password = document.querySelector('#password').value.trim();
 
-    //fetch should equal route for logging in
     const response = await fetch('api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
@@ -22,5 +20,4 @@ const loginForm = async function (event) {
     }
 }
 
-//querySelector should equal login form
 document.querySelector('#login').addEventListener('submit', loginForm);
